5c9e245317297618defd003b083f8ae66223e036
lucascardosoroscoe <lucascardosoroscoe@gmail.com>
2021-04-09 16:29:19 -0400

    Innitial

---
+++ contracts/token/BTP/BTP.sol
@@ -0,0 +1,13 @@
+
+pragma solidity ^0.8.0;
+
+import "../ERC20/ERC20.sol";
+
+
+contract BTP{
+    string  public constant _name;
+    string public constant _symbol;
+    uint8 public constant _decimal
+
+
+}
